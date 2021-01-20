7f688061e1b707e051ee1772596031023e9f0f4f
sarsa003 <deli.sarsar@gmail.com>
2021-01-20 11:43:27 -0500

    Allow CORS from anywhere

--- monitor-server/src/main/java/com/devops/tools/server/config/ServiceConfig.java
+++ monitor-server/src/main/java/com/devops/tools/server/config/ServiceConfig.java
@@ -3,6 +3,8 @@ package com.devops.tools.server.config;
 import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ServiceConfig {
@@ -10,6 +12,16 @@ public class ServiceConfig {
     @Bean
     public UrlValidator provideUrlValidator() {
         return UrlValidator.getInstance();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/graphql").allowedOrigins("*");
+            }
+        };
     }
 
 }
