9ce0edacd0df385afa43f580ce0ac60578eef661
sarsa003 <deli.sarsar@gmail.com>
2021-01-20 11:53:18 -0500

    Added CORS bean

--- monitor-server/src/main/java/com/devops/tools/server/config/ServiceConfig.java
+++ monitor-server/src/main/java/com/devops/tools/server/config/ServiceConfig.java
@@ -1,8 +1,12 @@
 package com.devops.tools.server.config;
 
 import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class ServiceConfig {
@@ -10,6 +14,20 @@ public class ServiceConfig {
     @Bean
     public UrlValidator provideUrlValidator() {
         return UrlValidator.getInstance();
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> provideCorsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/graphql", corsConfiguration);
+        final FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        registrationBean.setOrder(0);
+        return registrationBean;
     }
 
 }
